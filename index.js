390f75da802ea11af32208424e005cfefd75feac
apoorv2908 <tiwari0829t@gmail.com>
2023-06-03 17:55:26 +0530

    dom project added

---
+++ index.js
@@ -0,0 +1,42 @@
+let ranNum = parseInt(Math.random()* 100 + 1)
+
+let btn=document.getElementById("btn")
+
+let userInput = document.getElementById("userInput")
+
+let questionMark =document.getElementById("questionMark")
+
+let body = document.getElementsByTagName("body")
+
+let stratguesstext =document.getElementById("scor")
+
+let reloadgame =document.getElementById('reloadgame')
+
+let initialscore=100
+let highscore=null;
+
+btn.addEventListener("click",function(){
+
+    userInput= document.getElementById("userInput").value
+
+    if(userInput == ranNum){
+        questionMark.innerText=`${ranNum}`;
+        scor.innerText="🤩 Hurray You Won !!! 🤩";
+        document.body.style.backgroundColor="green";
+        initialscore--
+        document.querySelector('#score').textContent= "💯 Chances: " + initialscore*0
+        highscore=initialscore
+        document.querySelector('#highscore').textContent= "🥇 Highscore: " + highscore
+    }
+    else if(userInput < ranNum){
+        scor.innerText="Your Guess Is Low";
+        initialscore--
+        document.querySelector('#score').textContent= "💯 Chances: " + initialscore
+    }
+    else if (userInput>ranNum){
+        scor.innerText="Your Guess Is High";
+        initialscore--
+        document.querySelector('#score').textContent= "💯 Chances: " + initialscore
+
+    }
+})
